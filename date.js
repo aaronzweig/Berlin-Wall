ddb6ba4e8b4b03fccf555b58aca47c12c8644210
mrkidney <mrkidney@users.noreply.github.com>
2015-02-21 13:51:19 -0800

    Update date.js

--- date.js
+++ date.js
@@ -1,17 +1,20 @@
-	var evaluate = function(d, m, y, b){
-		return b*b*y + b*m + d;
-	}
+var evaluate = function(d, m, y, b){
+	return b*b*y + b*m + d;
+}
 
-	var date = new Date();
-	var day = date.getDate();
-	var month = date.getMonth() + 1;
-	var year = date.getFullYear();
-	var base = year + 1;
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+var base = year + 1;
 
-	var value = evaluate(day, month, year, base);
-	var begin = evaluate(1961, 8, 13, base);
-	var end = evaluate(1989, 11, 9, base);
+var value = evaluate(day, month, year, base);
+var begin = evaluate(13, 8 ,1961, base);
+var end = evaluate(9, 11, 1989, base);
 
-	if (value < begin) alert("before");
-	else if (value > end) alert("after");
-	else alert("during");
+var text;
+if (value < begin) text = "The Berlin Wall is down";
+else if (value > end) text = "The Berlin Wall is down";
+else text = "The Berlin Wall is up";
+
+document.getElementById("message").innerHTML = text;
