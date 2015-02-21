61d12d6836c611861ea641ca66355402bccf4b69
mrkidney <mrkidney@users.noreply.github.com>
2015-02-21 13:24:34 -0800

    Update date.js

--- date.js
+++ date.js
@@ -1,5 +1,17 @@
-var date = new Date();
-var day = date.getDay();
-var month = date.getMonth();
-var year = date.getFullYear();
-alert(day + " " + month + " " + year);
+	var evaluate = function(d, m, y, b){
+		return b*b*y + b*m + d;
+	}
+
+	var date = new Date();
+	var day = date.getDate();
+	var month = date.getMonth() + 1;
+	var year = date.getFullYear();
+	var base = year + 1;
+
+	var value = evaluate(day, month, year, base);
+	var begin = evaluate(1961, 8, 13, base);
+	var end = evaluate(1989, 11, 9, base);
+
+	if (value < begin) alert("before");
+	else if (value > end) alert("after");
+	else alert("during");
